de72c323b6fcd7e056baf170f6921896efa6dd3f
Shantun Tomar <75194520+shantuntomar@users.noreply.github.com>
2021-08-23 18:29:24 +0100

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -1,41 +1,38 @@
 class QuickSort {
 
-	public static void swap(int arr[] , int a , int b) {
-		int temp = arr[a];
-		arr[a] = arr[b];
-		arr[b] = temp;
-	} 
-
-	public static int partition(int arr[] , int l , int h) {
+	static int partition(int arr[] , int l , int h) {
 		int pivot = arr[h];
 		int i = (l-1);
-		for(int j=l; j<=h-1; j++) {
-			if(arr[j] < pivot) {
+		for(int j=l; j<h; j++) {
+			if(arr[j] <= pivot) {
 				i++;
-				swap(arr,i,j);
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
 		}
-		swap(arr,i+1,h);
+		int temp = arr[i+1];
+		arr[i+1] = arr[h];
+		arr[h] =temp;
+
 		return (i+1);
 	} 
 
-	public static void quickSort(int arr[] , int l , int h) {
-		if( l < h) {
-			int pi = partition(arr,l,h);
-			quickSort(arr,l,pi-1);
-			quickSort(arr,pi+1,h);
+	static void quicksort(int a[] , int l , int h) {
+		if(l<h) {
+			int pi = partition(a,l,h);
+			quicksort(a,l,pi-1);
+			quicksort(a,pi+1,h);
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		int arr[] = {9,8,7,6,5,4,3,2,1};
-		quickSort(arr , 0 , arr.length-1);
+		quicksort(arr, 0 , arr.length-1);
 
 		for(int ele : arr) {
 			System.out.print(ele+" ");	
 		}
 	}
 }
-
-
